2527c78c10da29e8faca076b83a1c8e19a43ae40
tamizhups <tloganathan@ups.com>
2025-12-12 11:33:13 +0530

    acommme

--- providers.tf
+++ providers.tf
@@ -12,7 +12,7 @@ provider "google" {
     region  = "us-central1"
   
 }
-# added 
+# added again
 resource "google_storage_bucket" "action1" {
     name     = " test-buck-786546"
     location = "US"
