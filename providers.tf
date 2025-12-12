1cfc399b7033b4ad6ddd4a9d4af47342cd779a27
tamizhups <tloganathan@ups.com>
2025-12-12 10:43:15 +0530

    added providers

---
+++ providers.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  backend "gcs" {
+    bucket = "test-buck-786545"
+    prefix = "terraform/state"
+    }
+}
+
+
+provider "google" {
+    project = "las-cloudlabs14"
+    region  = "us-central1"
+  
+}
+resource "google_storage_bucket" "action1" {
+    name     = " test-buck-786546"
+    location = "US"
+  
+}
